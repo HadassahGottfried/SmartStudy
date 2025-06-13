a09c4d2478cf640f5462c3e6be4a77a5de2689cd
HadassahGottfried <hadasa3281@gmail.com>
2025-06-13 09:42:50 +0300

    small fix in the file

--- backend/src/middlewares/rateLimitMiddleware.ts
+++ backend/src/middlewares/rateLimitMiddleware.ts
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit';
 
 export const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 דקות
-  max: 100, // 100 בקשות לכל IP בכל 15 דקות
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
   message: 'Too many requests from this IP, please try again later',
 });
